ec68d8f97d07438070e2ef0fcef7d90be083b1d6
Rado Nirina Ranaivosoa <rado.ranaivosoa@cnaps.mg>
2021-12-21 16:25:11 +0300

    Premier déploiement sur Heroku

--- app/__init__.py
+++ app/__init__.py
@@ -3,13 +3,21 @@
 from flask import Flask
 from flask_restx import Api
 
+from .db import tweet_repository
+from .models import Tweet
+
+tweet_repository.add(Tweet("a first tweet"))
+tweet_repository.add(Tweet("a second tweet"))
+
+
 def create_app():
     app = Flask(__name__)
 
     from .apis.tweets import api as tweets
+
     api = Api()
     api.add_namespace(tweets)
     api.init_app(app)
 
-    app.config['ERROR_404_HELP'] = False
+    app.config["ERROR_404_HELP"] = False
     return app
